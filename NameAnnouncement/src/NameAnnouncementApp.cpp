28fbd85e52e4ed47952c63c5b00c2fbb8dbcf906
ooHIROoo <hiro1215.turquoise@gmail.com>
2015-06-13 00:37:41 +0900

    テスト用json追加,stringからwstringに変換する関数追加

--- NameAnnouncement/src/NameAnnouncementApp.cpp
+++ NameAnnouncement/src/NameAnnouncementApp.cpp
@@ -2,9 +2,21 @@
 #include "Name.h"
 #include "cinder/gl/gl.h"
 #include "cinder\Camera.h"
+#include "cinder\Json.h"
 #include <unordered_map>
 #include <memory>
+#include <codecvt>
 
+//　stringをwstringに変換する関数
+//　@param {string} [str] - 変換したいマルチバイト文字列
+//　@param {wstring} [dest] - 変換するwstring
+void widen(const std::string& str,std::wstring& dest)
+{
+	wchar_t* wcs = new wchar_t[str.length() + 1];
+	mbstowcs(wcs, str.c_str(), str.length() + 1);
+	dest = wcs;
+	delete[]wcs;
+}
 
 using namespace ci;
 using namespace ci::app;
@@ -22,11 +34,23 @@ class NameAnnouncementApp : public AppNative {
 
 void NameAnnouncementApp::setup()
 {
-	_camera.setPerspective(90, getWindowAspectRatio(), 0.5f, 1000.0f);
-	_camera.lookAt(Vec3f::zAxis() * -10, Vec3f::zero(), Vec3f::yAxis());
+	setlocale(LC_CTYPE, "");
 
 	Name::setFont("naguri.ttf", 200);
-	_namemap.emplace(L"新井大一", std::make_shared<Name>(L"新井大一"));
+
+	JsonTree load(loadAsset("test.json"));
+
+	const auto& objects = load["Name"];
+
+	for (size_t i = 0; i < objects.getNumChildren();++i)
+	{
+		std::wstring wstr;
+		widen(objects[i].getValue<std::string>(), wstr);
+		_namemap.emplace(wstr, std::make_shared<Name>(wstr));
+	}
+
+	_camera.setPerspective(90, getWindowAspectRatio(), 0.5f, 1000.0f);
+	_camera.lookAt(Vec3f::zAxis() * -10, Vec3f::zero(), Vec3f::yAxis());
 
 }
 
@@ -53,10 +77,6 @@ void NameAnnouncementApp::draw()
 	{
 		name_object.second->draw();
 	}
-	//gl::drawStringCentered(
-	//	ci::toUtf8((char16_t*)std::wstring(L"新井大一").c_str()), 
-	//	Vec2f::zero(), ColorA::white(), font
-	//	);
 
 	gl::disableAlphaBlending();
 }
